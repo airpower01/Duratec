9361efbb7279a33a04942a86ac18c190abc591df
Air Power <141532102+airpower01@users.noreply.github.com>
2023-11-03 06:00:50 +0700

    Update CompanyProfile

--- duratech/src/components/CompanyProfile.vue
+++ duratech/src/components/CompanyProfile.vue
@@ -3,12 +3,10 @@
     OUR COMPANY PROFILE
   </p>
   <p class="text-center container" id="text-1" style="font-size: 15px">
-    Duratec adalah perusahaan hebat gan gaji besar, Duratec adalah perusahaan
-    hebat gan gaji besar, Duratec adalah perusahaan hebat gan gaji besar,
-    Duratec adalah perusahaan hebat gan gaji besar. Duratec adalah perusahaan
-    hebat gan gaji besar, Duratec adalah perusahaan hebat gan gaji besar,
-    Duratec adalah perusahaan hebat gan gaji besar, Duratec adalah perusahaan
-    hebat gan gaji besar, Duratec adalah perusahaan hebat gan gaji besar
+    <p>PT Duratec Power Indonesia offers our Indonesian customers range of air compressors and ancilarry products that are both durable and affordable </p>
+      <p>Air compressor has become a vital part of many industries since before, now, and in the coming years</p>
+      <p>Our mission is to serve the Indonesian businesses by delivering products and services that enable and supports for the growth of new and existing industries in Indonesia<p>
+      <p>We want to make a long term business relation with our customers, which is a fundamental concept where business exists to create value for all of its stakeholders. After all Happy customers, Happy employees. We value Trust and Accontabilities in our dealing and support our Group Vision and Code of Conducts</p>
   </p>
 </template>
 
